40bb8a0d2e729d85b9a9f398e29299efedf0fa17
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-01-24 16:00:55 +0100

    fixed some basic things

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,14 +5,7 @@ import {getFirestore} from "firebase/firestore"
 
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAygbEHZUBn80sXG4uHpjEES7PE-0H5o68",
-  authDomain: "rainergrenblog.firebaseapp.com",
-  projectId: "rainergrenblog",
-  storageBucket: "rainergrenblog.appspot.com",
-  messagingSenderId: "850399932968",
-  appId: "1:850399932968:web:3ac3df735d80146d73bb1d"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
